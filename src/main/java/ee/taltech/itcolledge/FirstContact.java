7cec649b842bab0b56ef005c7f0184eb807a5deb
Hayder <hayder.ha.ali@gmail.com>
2019-02-11 09:35:15 +0200

    Change from world to estonia

--- src/main/java/ee/taltech/itcolledge/FirstContact.java
+++ src/main/java/ee/taltech/itcolledge/FirstContact.java
@@ -3,6 +3,6 @@ package ee.taltech.itcolledge;
 public class FirstContact {
 
     public static void main(String[] args) {
-        System.out.println("Hello Estonia!");
+        System.out.println("Hello Estonia!!");
     }
 }
